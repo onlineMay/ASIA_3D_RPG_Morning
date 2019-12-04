06e34c3cafae6b024081329b2b2ad9c590597084
KID <50666207+KID421@users.noreply.github.com>
2019-12-04 11:32:10 +0800

    認識判斷是語法與規則

---
+++ UnityProject/Assets/Scripts/LearnIf.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LearnIf : MonoBehaviour
+{
+    private void Start()
+    {
+        // if (布林值) { 陳述式或演算法 }
+        // () 內的布林值為 true 會執行 {}
+        if (true)
+        {
+            print("測試!");
+        }
+    }
+}
