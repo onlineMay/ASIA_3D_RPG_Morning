11530919b03ed9655726565cdf034efe56d25512
KID <50666207+KID421@users.noreply.github.com>
2019-12-04 11:44:36 +0800

    判斷式關鍵字：否則

--- UnityProject/Assets/Scripts/LearnIf.cs
+++ UnityProject/Assets/Scripts/LearnIf.cs
@@ -2,6 +2,8 @@
 
 public class LearnIf : MonoBehaviour
 {
+    public bool openDoor;
+
     private void Start()
     {
         // if (布林值) { 陳述式或演算法 }
@@ -9,6 +11,20 @@ public class LearnIf : MonoBehaviour
         if (true)
         {
             print("測試!");
+        }
+    }
+
+    private void Update()
+    {
+        if (openDoor)
+        {
+            // if () 內的布林值為 true 會執行 if {}
+            print("開門");
+        }
+        else
+        {
+            // if () 內的布林值為 false 會執行 else {}
+            print("關門");
         }
     }
 }
