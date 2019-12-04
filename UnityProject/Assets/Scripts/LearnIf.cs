fdd69937d2af65023025affa1495b195a7dce3e7
KID <50666207+KID421@users.noreply.github.com>
2019-12-04 11:58:25 +0800

    判斷式進階應用：else if

--- UnityProject/Assets/Scripts/LearnIf.cs
+++ UnityProject/Assets/Scripts/LearnIf.cs
@@ -4,6 +4,8 @@ public class LearnIf : MonoBehaviour
 {
     public bool openDoor;
 
+    public int score;
+
     private void Start()
     {
         // if (布林值) { 陳述式或演算法 }
@@ -25,6 +27,23 @@ public class LearnIf : MonoBehaviour
         {
             // if () 內的布林值為 false 會執行 else {}
             print("關門");
+        }
+
+        if (score >= 60 )
+        {
+            print("及格");
+        }
+        else if (score >= 50)
+        {
+            print("可以補考!");
+        }
+        else if (score >= 40)
+        {
+            print("付錢來補考~");
+        }
+        else
+        {
+            print("被當惹");
         }
     }
 }
