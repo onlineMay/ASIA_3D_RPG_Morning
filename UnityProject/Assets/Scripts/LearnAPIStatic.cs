efb446d2c51865b1aca9cbec92e68f548a37b720
KID <50666207+KID421@users.noreply.github.com>
2019-12-11 09:57:41 +0800

    認識 API 與使用靜態屬性

---
+++ UnityProject/Assets/Scripts/LearnAPIStatic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LearnAPIStatic : MonoBehaviour
+{
+    private void Start()
+    {
+        // 使用靜態成員
+        // 成員：屬性 properties (類似欄位 fields)
+        // 成員：方法 methods
+
+        // 取得靜態屬性語法：類別.屬性
+        // 設定靜態屬性語法：類別.屬性 = 值
+
+        print(Random.value);
+    }
+}
